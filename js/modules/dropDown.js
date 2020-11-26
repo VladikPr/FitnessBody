7bdda793f05145bb94a3935130b8f78d4317c114
Vladyslav Pryhara <vladikpr94@hotmail.com>
2020-11-26 11:51:38 +0100

    Club Select List

--- js/modules/dropDown.js
+++ js/modules/dropDown.js
@@ -3,16 +3,15 @@ const dropDown = () => {
     const selectList = document.querySelector('.club-select');
     const clubList = document.querySelector('.clubs-list ul');
 
-
     selectList.addEventListener('click', (e) => {
         const {target} = e;
 
         if (target.closest('.club-select')) {
-            clubList.classList.toggle('deactiveted');
+            clubList.classList.toggle('deactivated');
         }
 
         if (target.closest('li')){
-            clubList.classList.add('deactiveted');
+            clubList.classList.add('deactivated');
         }
     });
 
